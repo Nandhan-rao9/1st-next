a8b1149efaa818db086f4aa4bef91154d5c3ca78
Nandhan <nandhanrao99@gmail.com>
2024-11-18 13:54:56 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+import { BentoGridDemo } from './components/bento-grid-demo'  // adjust the import path as needed
+export default function Page() {
+  return (
+    <main>
+      <BentoGridDemo />
+    </main>
+  );
+}
